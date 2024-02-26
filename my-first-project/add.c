4c72a29a839fcb8eb59e51f54cc248039bdcff9a
hacker <harvar1449@gmail.com>
2024-02-26 23:21:56 +0530

    New file - add.c is added

---
+++ my-first-project/add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+{
+	void main()
+	{
+		int a,b;
+		printf("Enter a value = ", &a);
+		scanf(%d, a);
+		printf("Enter b value = ", &b);
+		scanf(%d, b);
+		add = a+b;
+		printf("The sum of given numbers = ", &add);
+		getch();
+		return(0);
+	}
+}
