be8759cbf86706e197a5457b39b2c554473f93ce
hacker <harvar1449@gmail.com>
2024-02-27 15:06:33 +0530

    add.c - code is modified

--- my-first-project/add.c
+++ my-first-project/add.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <conio.h>
+void main()
 {
-	void main()
-	{
-		int a,b;
-		printf("Enter a value = ", &a);
-		scanf(%d, a);
-		printf("Enter b value = ", &b);
-		scanf(%d, b);
-		add = a+b;
-		printf("The sum of given numbers = ", &add);
-		getch();
-		return(0);
-	}
+	clrscr();
+	int a,b,add;
+	printf("Enter a value = ");
+	scanf("%d",&a);
+	printf("Enter b value = ");
+	scanf("%d",&b);
+	add = a+b;
+	printf("The sum of given numbers = %d", &add);
+	getch();
+	return(0);
 }
